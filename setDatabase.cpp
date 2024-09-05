60cc947143cf63cb68f71525c713bd2d09f9b66e
Sergey Kazakov <138627096+IAmSergeyKazakov@users.noreply.github.com>
2024-09-06 00:30:38 +0300

    deleteDBInfo

--- setDatabase.cpp
+++ setDatabase.cpp
@@ -4,9 +4,9 @@
 void MainWindow::setDatabase()
 {
     db = QSqlDatabase::addDatabase("QMYSQL");
-    db.setHostName("92.53.96.245");
-    db.setPort(3306);
-    db.setUserName("cf45733_kazak");
-    db.setPassword("RBBT9SWh");
-    db.setDatabaseName("cf45733_kazak");
+    db.setHostName("*ip*");
+    db.setPort(*port*);
+    db.setUserName("*userName*");
+    db.setPassword("*password*");
+    db.setDatabaseName("*dbName*");
 }
